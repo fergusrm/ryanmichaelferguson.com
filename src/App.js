480fde5343254a43373fbb7a27b9b66693954fdb
Ryan Ferguson <fergu189@gmail.com>
2020-01-08 11:16:52 -0700

    Set up project, routing, and css modules via sass

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Resume from './components/Resume';
+import './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/resume">Resume</NavLink>
+        <Route exact path="/" component={Home}></Route>
+        <Route path="/about" component={About}></Route>
+        <Route path="/resume" component={Resume}></Route>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Resume.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Home extends React.Component {
+    render() {
+        return <div className="App">
+        <header className="App-header">
+          <p>Resume for Ryan Ferguson</p>
+        </header>
+      </div>;
+    }
+}
+
+export default Home;
